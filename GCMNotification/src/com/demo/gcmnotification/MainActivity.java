f1c4e47ad021572f3cb3f62cf9ec3e37450a2a18
cindy <zhi.cao@landesk.com>
2014-03-17 16:41:04 +0800

    update

--- GCMNotification/src/com/demo/gcmnotification/MainActivity.java
+++ GCMNotification/src/com/demo/gcmnotification/MainActivity.java
@@ -23,6 +23,9 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.util.Log;
 import android.view.Menu;
 import android.widget.TextView;
@@ -33,8 +36,9 @@ public class MainActivity extends Activity {
 	public static final String EXTRA_MESSAGE = "message"; 
 	//registration_id will be storing the id with which android device gets register with GCM.
 	public static final String PROPERTY_REG_ID = "registration_id"; 
+	private static final String PROPERTY_APP_VERSION = "appVersion";
 	//SENDER_ID is the project number, which you get from google code website.
-	String SENDER_ID = "341663712821";
+	String SENDER_ID = "360831567396";
 	
 	//Define the request code sent to Google Play services
 	private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
@@ -63,26 +67,102 @@ public class MainActivity extends Activity {
 		//helps to connect to google cloud using background thread.
 		if(isPlayServicesAvaliable()){
 			//new RegisterBackground().execute();
-			
-			String msg = "";
-			try {
-				Log.e(TAG, "doInBackground");
-                if (gcm == null) {
-                	Log.e(TAG, "gcm == null");
-                    gcm = GoogleCloudMessaging.getInstance(context);
-                }
-                Log.e(TAG, "gcm.register(SENDER_ID)");
-                regid = gcm.register(SENDER_ID);
-                Log.e(TAG, regid);
-                msg = "Dvice registered, registration ID=" + regid;
-                Log.d(TAG, msg);
-                //sendRegistrationIdToBackend(regid);
+	        regid = getRegistrationId(context);
 
-            } catch (Exception ex) {
-                msg = "Error :" + ex.getMessage();
-                Log.e(TAG, msg);
-            }
+	        if (regid.isEmpty()) {
+	            Log.e(TAG, "registering in background");
+	            //new RegisterBackground().execute();
+				String msg = "";
+				try {
+					Log.e(TAG, "doInBackground");
+	                if (gcm == null) {
+	                	Log.e(TAG, "gcm == null");
+	                    gcm = GoogleCloudMessaging.getInstance(context);
+	                }
+	                Log.e(TAG, "gcm.register(SENDER_ID)");
+	                regid = gcm.register(SENDER_ID);
+	                Log.e(TAG, regid);
+	                msg = "Dvice registered, registration ID=" + regid;
+	                Log.d(TAG, msg);
+	                //sendRegistrationIdToBackend(regid);
+	                storeRegistrationId(context, regid);
+	            } catch (Exception ex) {
+	                msg = "Error :" + ex.getMessage();
+	                Log.e(TAG, msg);
+	            }
+	        } else {
+	            Log.e(TAG, "Notification Token : " + regid);
+	            //user.setNotificationToken(regid);
+	        }
 		}
+	}
+	
+	/**
+	 * Gets the current registration ID for application on GCM service.
+	 * <p>
+	 * If result is empty, the app needs to register.
+	 *
+	 * @return registration ID, or empty string if there is no existing
+	 *         registration ID.
+	 */
+	private String getRegistrationId(Context context) {
+	    final SharedPreferences prefs = getGCMPreferences(context);
+	    String registrationId = prefs.getString(PROPERTY_REG_ID, "");
+	    if (registrationId.isEmpty()) {
+	        Log.i(TAG, "Registration not found.");
+	        return "";
+	    }
+	    // Check if app was updated; if so, it must clear the registration ID
+	    // since the existing regID is not guaranteed to work with the new
+	    // app version.
+	    int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
+	    int currentVersion = getAppVersion(context);
+	    if (registeredVersion != currentVersion) {
+	        Log.i(TAG, "App version changed.");
+	        return "";
+	    }
+	    return registrationId;
+	}
+	
+	/**
+	 * Stores the registration ID and app versionCode in the application's
+	 * {@code SharedPreferences}.
+	 *
+	 * @param context application's context.
+	 * @param regId registration ID
+	 */
+	private void storeRegistrationId(Context context, String regId) {
+	    final SharedPreferences prefs = getGCMPreferences(context);
+	    int appVersion = getAppVersion(context);
+	    Log.i(TAG, "Saving regId on app version " + appVersion);
+	    SharedPreferences.Editor editor = prefs.edit();
+	    editor.putString(PROPERTY_REG_ID, regId);
+	    editor.putInt(PROPERTY_APP_VERSION, appVersion);
+	    editor.commit();
+	}
+	
+	/**
+	 * @return Application's version code from the {@code PackageManager}.
+	 */
+	private static int getAppVersion(Context context) {
+	    try {
+	        PackageInfo packageInfo = context.getPackageManager()
+	                .getPackageInfo(context.getPackageName(), 0);
+	        return packageInfo.versionCode;
+	    } catch (NameNotFoundException e) {
+	        // should never happen
+	        throw new RuntimeException("Could not get package name: " + e);
+	    }
+	}
+
+	/**
+	 * @return Application's {@code SharedPreferences}.
+	 */
+	private SharedPreferences getGCMPreferences(Context context) {
+	    // This sample app persists the registration ID in shared preferences, but
+	    // how you store the regID in your app is up to you.
+	    return getSharedPreferences(MainActivity.class.getSimpleName(),
+	            Context.MODE_PRIVATE);
 	}
     
 	private boolean isPlayServicesAvaliable() {
@@ -136,7 +216,7 @@ public class MainActivity extends Activity {
                 msg = "Dvice registered, registration ID=" + regid;
                 Log.d(TAG, msg);
                 sendRegistrationIdToBackend(regid);
-
+                storeRegistrationId(context, regid);
             } catch (IOException ex) {
                 msg = "Error :" + ex.getMessage();
                 Log.e(TAG, msg);
@@ -151,6 +231,7 @@ public class MainActivity extends Activity {
             mDisplay.append(msg + "\n");
 
         }
+		
 		private void sendRegistrationIdToBackend(String regid) {
             // this code will send registration id of a device to our own server.
 			String url = "http://www.yourwebsitename.com/getregistrationid.php";
